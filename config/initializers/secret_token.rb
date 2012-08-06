7fa2aab330e678bebc2c47dfddf478c59a016bb2
Wiedenbruch-Invision <detlef.wiedenbruch@invision.de>
2012-08-06 20:46:18 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testmanager::Application.config.secret_token = '1ef7fe7aa8ed95d4d54653e0c6a85ac19a481f59ce71b71e1864d0fb984f6810a582b7bb4f217ce20df028a5cdda3031bd09d9473c7d0f941d019f1a95d0e77b'
